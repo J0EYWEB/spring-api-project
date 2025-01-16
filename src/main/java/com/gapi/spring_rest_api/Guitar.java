c06769c453736cd74b8a273511d06bd9d21a4287
Josef Webster <joeywebster97@gmail.com>
2025-01-16 19:35:33 +0000

    feat(api): Configure MySQL database

--- src/main/java/com/gapi/spring_rest_api/Guitar.java
+++ src/main/java/com/gapi/spring_rest_api/Guitar.java
@@ -1,16 +1,21 @@
 package com.gapi.spring_rest_api;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Positive;
 import lombok.*;
 
-
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Guitar {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotEmpty(message = "Make is required")
